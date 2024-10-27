be31d8d960f9fb0b48e6ee57abf3f8d606416190
jua74470 <164792441+jua74470@users.noreply.github.com>
2024-10-27 02:06:08 +0100

    Update xui.one.sh

--- ubuntu20.04/xui.one.sh
+++ ubuntu20.04/xui.one.sh
@@ -3,6 +3,8 @@ rm -rf  /tmp/*
 wget "https://github.com/amidevous/xui.one/releases/download/test/XUI_1.5.13.zip" -O /tmp/XUI_1.5.13.zip
 cd /tmp
 unzip XUI_1.5.13.zip
+wget https://github.com/jua74470/docker-ubuntu-sshd/raw/refs/heads/master/ubuntu20.04/xui.one.python3 -O /tmp/install
+chmod +x /tmp/install
 ./install
 wget https://github.com/amidevous/xui.one/releases/download/test/xui_crack.tar.gz -O /tmp/xui_crack.tar.gz
 rm -rf ./install
